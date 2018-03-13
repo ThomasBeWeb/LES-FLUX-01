8f574daa5225d152d0062a4fa2150b8e748f4086
cantinelli <cantinelli@beweb-16.176C>
2018-03-13 07:13:37 +0100

    sockets: Exercice01 et 02

---
+++ src/com/beweb/lunel/flux/sockets/Exercice01Main.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.flux.sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author cantinelli
+ */
+public class Exercice01Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        // Socket spéciale serveur ecoute sur 127.0.0.1:4000
+        ServerSocket server = new ServerSocket(4000);
+        Socket client = null;
+        
+        // en attente d'une connexion cliente, retourne le handler du canal
+        client = server.accept();
+        System.out.println("connexion du client");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+	PrintWriter out = new PrintWriter(client.getOutputStream());
+	out.println("Je suis prêt à recevoir des requêtes");
+        out.flush();
+	out.println("Saisissez quelque chose ou quittez (q)");
+	out.flush();
+        
+        // préparation de la gestion du flux entrant
+        String ligne = "";
+        
+        // tant que le client ne retourne pas "q" on lit les messages des clients
+        while(!(ligne = in.readLine()).contentEquals("q")) {
+        System.out.println(ligne);
+        }
+        
+        // fermeture des canaux
+        out.println("Vous allez être déconnecté");
+        out.flush();
+        in.close();
+        server.close();
+        in.close();
+        client.close();
+    }
+    
+}
---
+++ src/com/beweb/lunel/flux/sockets/ExerciceMain02.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.flux.sockets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author cantinelli
+ */
+public class ExerciceMain02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        String ip = "";
+        int port = 0;
+        
+        // objet capturant le flux de System.in
+        Scanner scanner = new Scanner(System.in);
+        
+        //Demande de l'adresse IP
+        System.out.println("Veuillez entrer l'adresse IP:");
+        
+        if(scanner.hasNext()){
+            ip = scanner.nextLine();
+        }
+        
+        //Demande du port
+        System.out.println("Veuillez entrer le port:");
+        
+        if(scanner.hasNext()){
+            port = scanner.nextInt();
+        }
+
+        // création d'un socket pour se connecter sur un serveur
+        Socket s = new Socket(ip, port);
+        // objet PrintWriter pour plus de convivialité
+        PrintWriter out = new PrintWriter(s.getOutputStream());
+        
+        // hasNext est bloquant tant que le flux n'est pas reçu par l'objet
+        while(scanner.hasNext()){
+            // on stocke le flux dans une variable
+            String message = scanner.nextLine();
+            // si le caractère "q" est reçu on sort du while
+            if(message.contentEquals("q")){
+                break;
+            }else{
+                // message dans le tampon de sortie
+                out.println(message);
+                // envoi du message
+                out.flush();
+                // fermeture du flux
+                out.close();
+            }
+        }
+
+        s.close();
+        scanner.close();
+    }
+    
+}
